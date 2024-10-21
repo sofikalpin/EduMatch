cba9b04779e5bfff5d56c1aeec62e17ee23042a4
Oriacosta <orianaantonellaacosta@alu.frlp.utn.edu.ar>
2024-10-20 21:11:36 -0300

    front

---
+++ frontend-sistema-apoyo/frontend-sistema-apoyo/src/componentes/inicio/Inicio.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Inicio.css'; 
+import logo from '../../logo/LogoFinal.png';
+import { Link } from 'react-router-dom'; 
+import { Login } from './../login/Login';
+
+export const Inicio = () => {
+    return (
+        <div className="inicio-container">
+            <header className="header">
+                <img src={logo} alt="Logo" className="logo-img" />
+                <nav className="navigation">
+                    <ul>
+                        <li><Link to="#">Profesores</Link></li>
+                        <li><Link to="#">Programa</Link></li>
+                        <li><Link to="#">Herramientas</Link></li>
+                    </ul>
+                </nav>
+                <div className="auth-buttons">
+                <Link to="/login" className="login-button">Login</Link> 
+                    <Link to="/registrarse" className="register-button">Registrate</Link> 
+                </div>
+            </header>
+            <div className="search-container">
+                <input type="text" placeholder="Buscar..." className="search-input" />
+                <button className="search-button">Buscar</button>
+            </div>
+            <section className="intro">
+                <p>Estos son nuestros cursos de inglés listos para que empieces a aprender.</p>
+            </section>
+        </div>
+    );
+};
+
+export default Inicio;

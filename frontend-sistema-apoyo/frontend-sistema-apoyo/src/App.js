3a7826c1f655b1c9f08aef88e46498fdf4a6a666
sofikalpin <sofikalpin@hotmail.com>
2024-12-12 19:39:22 -0300

    arreglado

--- frontend-sistema-apoyo/frontend-sistema-apoyo/src/App.js
+++ frontend-sistema-apoyo/frontend-sistema-apoyo/src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
-import './App.css';
-
-import Inicio from './componentes/inicio/Inicio'; // Si Inicio usa export default
-import Login from "./componentes/login/Login";
 import { Routes, Route } from "react-router-dom";
-import { Registrar } from "./componentes/registrar/Registrar"; // Si Registrar usa export named
-import { Perfil } from "./componentes/perfil/Perfil"; // Si Perfil usa export named
+import "./App.css";
+
+import Inicio from "./componentes/inicio/Inicio";
+import Login from "./componentes/login/Login"; // Verifica que este archivo exista y esté en esta ruta
+import { Registrar } from "./componentes/registrar/Registrar"; 
+import { Perfil } from "./componentes/perfil/Perfil"; 
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Inicio />} />
-        <Route path="login" element={<Login />} />
-        <Route path="registrarse" element={<Registrar />} />
-        <Route path="perfil" element={<Perfil />} />
+        <Route path="/login" element={<Login />} /> {/* Asegúrate de usar "/" en las rutas */}
+        <Route path="/registrarse" element={<Registrar />} />
+        <Route path="/perfil" element={<Perfil />} />
       </Routes>
     </div>
   );
--- frontend-sistema-apoyo/frontend-sistema-apoyo/src/Services/CursoService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getCursos = async () => {
-  const response = await axios.get('/api/profesor/cursos');
-  return response.data;
-};
